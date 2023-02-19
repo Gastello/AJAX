6828741bdecbe6e1dd5b681d20ba29fae80777d8
Gastello <maximgst@gmail.com>
2023-02-19 17:15:46 +0200

    get neighbours function

--- project/js/main.js
+++ project/js/main.js
@@ -1,54 +1,67 @@
-const container = document.querySelector('.country__wrapper');
+const container = document.querySelector('.countries');
 
-function getCountryData(country) {
+
+function getCountryData(country, isParent = true, isEnemy = false) {
+    let additionalClass = "";
+    const nameLink = `https://restcountries.com/v3.1/name/`;
+    const codeLink = `https://restcountries.com/v3.1/alpha/`;
     const request = new XMLHttpRequest();
-    console.log(country);
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-    request.send(); 
+    if (isParent) {
+        request.open('GET', `${nameLink}${country}`);
+    } else {
+        request.open('GET', `${codeLink}${country}`);
+        additionalClass = 'country__neighbour';
+    }
+    request.send();
     request.addEventListener('load', function () {
-        let [data] = JSON.parse(this.responseText)
-        console.log(data);
-        let language = Object.values(data.languages)[0];
-        let currencyName = Object.values(data.currencies)[0].name;
-        let htmlCard = `
-        <article class="country">
-        <div class="country__img">
-            <img src="${data.flags.png}" alt="">
-        </div>
-        <div class="country__data">
-        <h3 class="country__name">
-            ${data.name.official}
-        </h3>
-        <h4 class="country__region">
-            ${data.region}
-        </h4>
-        <div class="country__column">
-            <p class="country__row">
-                <span>👨‍👩‍👦‍👦</span>
-                <span>${(data.population / 1000000).toFixed(2)} млн</span>
-            </p>
-            <p class="country__row">
-                <span>🌐</span>
-                <span>${language}</span>
-            </p>
-            <p class="country__row">
-                <span>💸</span>
-                <span>${currencyName}</span>
-            </p>
-        </div>
-        </div>
-        </article> 
-    `;
+        const [data] = JSON.parse(this.responseText);
+        const language = Object.values(data.languages)[0];
+        const currencyName = Object.values(data.currencies)[0].name;
+        const htmlCard = `  
+            <div class="country__wrapper ${additionalClass}">
+                <article class="country">
+                    <div class="country__img">
+                        <img src="${isEnemy ? `https://cdn.drawception.com/images/panels/2012/5-14/Q1MzdmHO4X-14.png` : data.flags.png}" alt="">
+                    </div>
+                    <div class="country__data">
+                        <h3 class="country__name">
+                            ${isEnemy?`Shit Federation`:data.name.official}
+                        </h3>
+                        <h4 class="country__region">
+                            ${isEnemy?`Shit Region`:data.region}
+                        </h4>
+                        <div class="country__column">
+                            <p class="country__row">
+                            <span>👨‍👩‍👦‍👦</span>
+                            <span>${(data.population / 1000000).toFixed(2)} million ${isEnemy?` shit eaters`:''}</span>
+                            </p>
+                            <p class="country__row">
+                            <span>🌐</span>
+                            <span>${isEnemy?language +` shit language`:language}</span>
+                            </p>
+                            <p class="country__row">
+                            <span>💸</span>
+                            <span>${currencyName}</span>
+                            </p>
+                        </div>
+                    </div>
+                </article>
+                </div>
+        `;
         container.insertAdjacentHTML('beforeend', htmlCard);
+        const neighbours = data.borders;
+        if (isParent) getNeighbours(neighbours);
     });
 }
-getCountryData(`ukraine`);
-getCountryData(`usa`);
-getCountryData(`poland`);
-getCountryData(`australia`);
-getCountryData(`france`);
-getCountryData(`africa`);
-getCountryData(`netherland`);
-getCountryData(`canada`);
 
+function getNeighbours(neighbours) {
+    neighbours.forEach(country => {
+        if (country == 'RUS') {
+            getCountryData(country, false, true);
+        }
+        else getCountryData(country, false);
+    });
+}
+
+getCountryData(`ukraine`); 
 
